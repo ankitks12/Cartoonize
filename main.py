f3a3df3a003749e0d56f99a864af2d6eaa3e5aa9
Ankit Kumar Singh <chhotu12ank@gmail.com>
2020-08-05 23:24:38 +0530

    DL model deployed

---
+++ main.py
@@ -0,0 +1,109 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf 
+import network
+import guided_filter
+from tqdm import tqdm
+
+'''from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np'''
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+
+def resize_crop(image):
+    h, w, c = np.shape(image)
+    if min(h, w) > 720:
+        if h > w:
+            h, w = int(720*h/w), 720
+        else:
+            h, w = 720, int(720*w/h)
+    image = cv2.resize(image, (w, h),
+                       interpolation=cv2.INTER_AREA)
+    h, w = (h//8)*8, (w//8)*8
+    image = image[:h, :w, :]
+    return image
+    
+
+def cartoonize(load_folder, save_folder, model_path):
+    input_photo = tf.placeholder(tf.float32, [1, None, None, 3])
+    network_out = network.unet_generator(input_photo)
+    final_out = guided_filter.guided_filter(input_photo, network_out, r=1, eps=5e-3)
+
+    all_vars = tf.trainable_variables()
+    gene_vars = [var for var in all_vars if 'generator' in var.name]
+    saver = tf.train.Saver(var_list=gene_vars)
+    
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+
+    sess.run(tf.global_variables_initializer())
+    saver.restore(sess, tf.train.latest_checkpoint(model_path))
+    name_list = os.listdir(load_folder)
+    for name in tqdm(name_list):
+        try:
+            load_path = os.path.join(load_folder, name)
+            save_path = os.path.join(save_folder, name)
+            image = cv2.imread(load_path)
+            image = resize_crop(image)
+            batch_image = image.astype(np.float32)/127.5 - 1
+            batch_image = np.expand_dims(batch_image, axis=0)
+            output = sess.run(final_out, feed_dict={input_photo: batch_image})
+            output = (np.squeeze(output)+1)*127.5
+            output = np.clip(output, 0, 255).astype(np.uint8)
+            cv2.imwrite(save_path, output)
+        except:
+            print('cartoonize {} failed'.format(load_path))
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'test_images', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        model_path = os.path.join(basepath, 'saved_models')
+        load_folder = os.path.join(basepath, 'test_images')
+        save_folder = os.path.join(basepath, 'cartoonized_images')
+        if not os.path.exists(save_folder):
+          os.mkdir(save_folder)
+        cartoonize(load_folder, save_folder, model_path)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        #pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        #result = str(pred_class[0][0][1])               # Convert to string
+        result_list = os.listdir(load_folder)
+        result = os.path.join(load_folder, result_list[0])
+        return result
+    return None
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
